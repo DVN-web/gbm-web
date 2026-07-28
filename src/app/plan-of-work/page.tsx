293c3f309b39deb7cc18e9a56c729a147aa068d1
Gagauz-bilim <researchcentr.gag@gmail.com>
2026-07-28 11:07:35 +0300

    add gender equality plan ru

--- src/app/plan-of-work/page.tsx
+++ src/app/plan-of-work/page.tsx
@@ -66,7 +66,7 @@ export default function ManagementStructure(){
                     </div>
                     <div>
                         <h3> УСТАВ Научно-исследовательского центра Гагаузии им. М.В. Маруневич <a href="https://cloud.mail.ru/public/MpxS/BSsvBdqVw">Скачать</a></h3>
-                        
+                        <h3> ПЛАН ГЕНДЕРНОГО РАВЕНСТВА (ПГР) <a href="/plan/gender-equality-ru.pdf">Скачать</a></h3>
                     </div>
                     <div className='otcetList'>
                         {planArr.map(obj=>(
